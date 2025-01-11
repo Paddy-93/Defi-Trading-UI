0e00792a8d8ab0db47452c50585c31e0d0df0b21
Paddy-93 <patrick.odriscoll05@gmail.com>
2025-01-11 17:32:16 -0500

    Test pipeline

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p className="text-4xl">
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.tsx</code> ad.
         </p>
         <a
           className="App-link"
